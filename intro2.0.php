84fc1300ea3c19c568564a362ae9828d7b185731
lucasarg2023 <lopezlucasnqn@gmail.com>
2024-05-24 14:49:48 -0300

    borrador 2

---
+++ intro2.0.php
@@ -0,0 +1,66 @@
+<?php 
+
+
+class Ejemplo {
+
+
+    private $ ;
+    private $;
+    private $;
+    private $;
+    private $;
+    private $; 
+
+
+
+    public function __construct ($ , $, $,  $,  $, $ ){
+        $this -> c = $ ;
+        $this -> c = $ ;
+        $this -> c = $ ;
+        $this -> c = $ ;
+        $this -> c = $ ;
+        $this -> c = $ ;
+    }
+
+    // gets
+    public function get (){
+        return $this -> ;
+    }
+    public function get (){
+        return $this ->  ;
+    }
+    public function get (){
+        return $this ->  ;
+    }
+    public function get (){
+        return $this ->  ;
+    }
+    public function get (){
+        return $this ->  ;
+    }
+    public function get (){
+        return $this ->  ;
+    }
+  
+
+    // sets
+    public function set ($){
+        $this ->  = $ ;
+    }
+    public function set ($){
+        $this ->  = $ ;
+    }
+    public function set ($){
+        $this ->  = $ ;
+    }
+    public function set ($){
+        $this ->  = $ ;
+    }
+    public function set ($){
+        $this ->  = $ ;
+    }
+    public function set ($){
+        $this ->  = $ ;
+    }
+
+}
